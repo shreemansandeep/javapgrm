7cde0c2b8b9a52dadf4c6843059146ea4ec12d20
shreemansandeep <81627168+shreemansandeep@users.noreply.github.com>
2022-07-30 13:39:44 +0530

    Update hellojva.java

--- hellojva.java
+++ hellojva.java
@@ -4,6 +4,8 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+@WebServlet(name="hello",urlPatterns={"/hello"}) // added this line
+
 public class HelloWorld extends HttpServlet {
   public void doGet(HttpServletRequest request,
                     HttpServletResponse response)
